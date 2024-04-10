6d15ad0e539b15cca97abc9a8bc7c58ba7c0781e
Philip Daniel Keicher <philip.daniel.keicher@cern.ch>
2024-04-10 15:47:25 +0200

    entries for nanoaods from cms tau pog with additional information about tau decays

---
+++ cmsdb/campaigns/run2_2017_cms_taupog_nano_v12/__init__.py
@@ -0,0 +1,51 @@
+
+
+# coding: utf-8
+
+"""
+Common, analysis independent definition of the 2017 data-taking campaign
+with datasets at NanoAOD tier in version 11, created with custom content at UHH.
+See https://python-order.readthedocs.io/en/latest/quickstart.html#analysis-campaign-and-config.
+
+Dataset ids are identical to those of MiniAOD input datasets in DAS (https://cmsweb.cern.ch/das).
+
+Since this is a custom production, neither can LFNs be obtained through DAS (or dasgloclient), nor
+can PFNs be located through the usual, central redirectors.
+
+They are centrally saved at the EOS instance at CERN under
+/eos/cms/eos/cms/store/group/phys_higgs/HLepRare/HTT_skim_v1
+"""
+
+from order import Campaign
+
+
+#
+# campaign
+#
+
+run2_2017_cms_taupog_nano_v12 = Campaign(
+    name="run2_2017_cms_taupog_nano_v12",
+    id=220171220,  # 2 2017 12 20(t)
+    ecm=13,
+    bx=25,
+    aux={
+        "tier": "NanoAOD",
+        "year": 2017,
+        "version": 12,
+        "custom": {
+            "name": "cms_taupog",
+            "creator": "cms",
+            "wlcg_fs": "wlcg_fs_tau_pog_nanoaod",  # noqa
+            "tag": "HTT_skim_v1",
+        },
+    },
+)
+
+
+# trailing imports to load datasets
+# import cmsdb.campaigns.run2_2017_cms_taupog_nano_v12.data  # noqa
+import cmsdb.campaigns.run2_2017_cms_taupog_nano_v12.top  # noqa
+# import cmsdb.campaigns.run2_2017_cms_taupog_nano_v12.ewk  # noqa
+# import cmsdb.campaigns.run2_2017_cms_taupog_nano_v12.qcd  # noqa
+# import cmsdb.campaigns.run2_2017_cms_taupog_nano_v12.higgs  # noqa
+# import cmsdb.campaigns.run2_2017_cms_taupog_nano_v12.hh2bbtautau  # noqa
---
+++ cmsdb/campaigns/run2_2017_cms_taupog_nano_v12/top.py
@@ -0,0 +1,47 @@
+# coding: utf-8
+
+"""
+Top quark datasets for the 2017 data-taking campaign with datasets at NanoAOD tier in
+version 11, created with custom content at UHH.
+"""
+
+import cmsdb.processes as procs
+from cmsdb.campaigns.run2_2017_cms_taupog_nano_v12 import run2_2017_cms_taupog_nano_v12 as cpn
+
+
+#
+# ttbar
+#
+
+cpn.add_dataset(
+    name="tt_sl_powheg",
+    id=14186280,
+    processes=[procs.tt_sl],
+    keys=[
+        "/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL17NanoAODv11-106X_mc2017_realistic_v9-v1/NANOAODSIM",  # noqa
+    ],
+    n_files=223,
+    n_events=1, # meaningless number because I don't want to look it up
+)
+
+cpn.add_dataset(
+    name="tt_dl_powheg",
+    id=14188237,
+    processes=[procs.tt_dl],
+    keys=[
+        "/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL17NanoAODv11-106X_mc2017_realistic_v9-v1/NANOAODSIM",  # noqa
+    ],
+    n_files=92,
+    n_events=106724000, # meaningless number because I don't want to look it up
+)
+
+cpn.add_dataset(
+    name="tt_fh_powheg",
+    id=14222447,
+    processes=[procs.tt_fh],
+    keys=[
+        "/TTToHadronic_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL17NanoAODv11-106X_mc2017_realistic_v9-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=97,
+    n_events=235719999, # meaningless number because I don't want to look it up
+)
